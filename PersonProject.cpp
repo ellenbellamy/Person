941fa681c5a432fd5986562a7c7862f26f86e5c6
swirl unceasing <schetinina27elena@gmail.com>
2020-04-11 15:28:25 +0300

    Initial version

---
+++ PersonProject.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Person
+{
+    
+public:
+    Person(const string & firstNameString, const string & middleNameString, const string & lastNameString) {
+        firstName = firstNameString;
+        lastName = middleNameString;
+        middleName = lastNameString;
+    }
+
+    void print()
+    {
+        std::cout << firstName << middleName << lastName << std::endl;
+    }
+
+private:
+    string firstName;
+    string middleName;
+    string lastName;
+
+};
+
+int main()
+{
+    Person * person = new Person("Anna", "Mary", "Smith" );
+   
+    person -> print();
+    return 0;
+}
